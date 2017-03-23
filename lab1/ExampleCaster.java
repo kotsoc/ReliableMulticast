b320a7cde3a40a407d5e628e1d876d4914ffb566
Konstantinos Peratinos <konper@ed3507-16.ed3507.studat.rh66.ii.2.hb.s.cdal.chalmers.se>
2017-03-23 19:01:31 +0100

    Many Changes , changed prop counter , changed delivery, ontroduce 2nd msg history "

--- lab1/ExampleCaster.java
+++ lab1/ExampleCaster.java
@@ -11,10 +11,10 @@ public class ExampleCaster extends Multicaster {
      ArrayList<Integer> clock = new ArrayList<Integer>();
      ArrayList<Integer> propCount = new ArrayList<Integer>();
      ArrayList<Integer> agrSeq = new ArrayList<Integer>();
-     //TreeMap<Integer, String> msgHistory = new TreeMap<Integer, String>();
+     Queue<String> msgHistory = new LinkedList<String>();
 	 ArrayList<TreeMap<Integer,String>> msgHistory2 = new ArrayList<TreeMap<Integer, String>>();
-     int delivered  = 0;
-     int myClock,maxProp = 0;
+     int delivered  = 1;
+     int myClock,maxProp,maxSelf = 0;
      int cHosts=3;
     /**
      * No initializations needed for this simple one
@@ -57,17 +57,16 @@ public class ExampleCaster extends Multicaster {
 		int max = 0;
 		// We propose a sequence number for the message		
 		if (((ExampleMessage)message).proposed == 0 ){
-			for (int i : clock){
-				if (i >= max){// Calculating proposal
-					max=i;				
-				}
+			if (msgClock < maxSelf){// Calculating proposal
+					max = maxSelf;
+					maxSelf++;				
 			}
 			if (max<maxProp){
 				max=maxProp;
 			}
 			bcom.basicsend(sender,new ExampleMessage(sender, ((ExampleMessage)message).text,((ExampleMessage)message).timestamp,max+1));
 			clock.set(sender,msgClock);
-			maxProp = max+1;
+			maxProp= max+1;
 		}
 		else{ 
 			System.out.println(sender);
@@ -75,21 +74,26 @@ public class ExampleCaster extends Multicaster {
 				if (propCount.get(msgClock-1) < hosts-1){
 					if (((ExampleMessage)message).proposed >= agrSeq.get(msgClock-1)){				
 					agrSeq.set(msgClock-1,((ExampleMessage)message).proposed);
-					System.out.println("agrSeq: " + agrSeq);
 					}
 				propCount.set(msgClock-1,(propCount.get(msgClock-1))+1);
 				System.out.println("PropCount: "+ propCount);
 				if (propCount.get(msgClock-1) == cHosts -1){
+						if (agrSeq.get(msgClock-1) <= maxSelf){
+							maxSelf= maxSelf+1;
+							agrSeq.set(msgClock-1,maxSelf);							
+						}
 						for(int i=0; i < hosts; i++) {
             					/* Sends to everyone except itself */
 		        			if(i != id) {
 		           				bcom.basicsend(i,new ExampleMessage(id, ((ExampleMessage)message).text,((ExampleMessage)message).timestamp,agrSeq.get(msgClock-1)));
+								maxSelf = agrSeq.get(msgClock-1);
 		        			}
 						}
+						System.out.println("agrSeq: " + agrSeq);
 						maxProp = agrSeq.get(msgClock-1);
 						//msgHistory.put(((ExampleMessage)message).proposed,((ExampleMessage)message).text);
-						msgHistory2.get(id).put(((ExampleMessage)message).proposed,((ExampleMessage)message).text);
-						deliver(agrSeq.get(msgClock-1),sender);
+						msgHistory2.get(id).put((agrSeq.get(msgClock-1)),((ExampleMessage)message).text);
+						deliver(agrSeq.get(msgClock-1),id);
 					}							
 				}
 				else{
@@ -102,27 +106,71 @@ public class ExampleCaster extends Multicaster {
 				deliver(((ExampleMessage)message).proposed,sender);	
 			}
 		}
-		System.out.println(clock);
+		if (myClock< 9){
+		    System.out.println(clock);
+		}
+		else {
+		    System.out.println(msgHistory2.get(id).entrySet());
+		}
     }
 
-
-
-		public void deliver(int seq,int ident){		
-			System.out.println("message out of sequence");
-			for(Map.Entry<Integer,String> entry : msgHistory2.get(ident).entrySet()){
-				System.out.println("mEdw 1");
-				if (entry.getKey()<= seq){
-					for(int i=0;i<cHosts;i++){ 
-						if (msgHistory2.get(i).containsValue(entry.getValue()) ){
-						mcui.deliver(i, msgHistory2.get(i).remove(seq), "from"+ Integer.toString(i));
-						}						
+	public void deliver(int seq,int ident){		
+		Integer min = 1;
+		int j = ident;
+		boolean found = false;
+		while (min <=seq){
+			for(int i=0;i<cHosts;i++){
+				for(Map.Entry<Integer,String> entry : msgHistory2.get(i).entrySet()){
+					if (entry.getKey()<= min){
+						j = i;
+						min = entry.getKey();
+						msgHistory.add(msgHistory2.get(j).remove(min));
+						found= true;	
+					}
+					else{
+						break;
 					}
 				}
+			}
+			if (!msgHistory2.get(j).containsKey(min) && found == true ){
+				for(int i=0;i<=msgHistory.size();i++){
+					mcui.deliver(i, msgHistory.poll(), "from"+ Integer.toString(i));
+					System.out.println("Message Seq:"+ min);
+				}
+				min = min++;
+				found = false;
+			}					
+			else{
+				min = min +1;				
+			}
+		}
+	}
+
+
+
+	/*public void deliver(int seq,int ident){
+		boolean found = true;
+		while (delivered <= seq)	
+		for(int i=0;i<cHosts;i++){
+			for(Map.Entry<Integer,String> entry : msgHistory2.get(i).entrySet()){
+				if (entry.getKey()== delivered){
+					msgHistory.add(msgHistorymsgHistory2.get(i).remove(delivered));
+				}
 				else{
-					break;				
+					break;
+				}
+		if (msgHistory2.get(i).containsKey(delivered)){
+			mcui.deliver(i, msgHistory2.get(i).remove(delivered), "from"+ Integer.toString(i));
+			System.out.println("Message Seq:"+ delivered);
+			found = true;
+					}						
 				}
 			}
-		}	
+			if (i==cHosts && found){
+				delivered++;
+			}
+		}					
+	}*/
 
 
     /**
